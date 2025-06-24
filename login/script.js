1799383cdc8d1ec3254a8e92fa2b6e4641f1a9d9
Gizza F Nadifa <geeznotgizz@gmail.com>
2025-06-24 11:45:58 +0700

    Update script.js

--- login/script.js
+++ login/script.js
@@ -32,8 +32,6 @@ if (document.getElementById('loginForm')) {
       const sessionData = { username: uname, loginTime: Date.now() };
       sessionStorage.setItem('session', JSON.stringify(sessionData));
       setCookie('session_user', uname, 1); // expire in 1 day
-
-      window.location.href = 'supa.html';
     } else {
       alert('Username atau password salah!');
     }
